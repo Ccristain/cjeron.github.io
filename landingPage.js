c348090106b32887bb27da8f8602db9e46e125d6
Ccristain <100702993+Ccristain@users.noreply.github.com>
2025-12-13 01:55:32 +0800

    Add files via upload

---
+++ landingPage.js
@@ -0,0 +1,26 @@
+const navBtn = document.getElementById("nav-btn");
+const header = document.getElementById("header");
+
+let isOn = false;
+
+navBtn.addEventListener("click", (e) => {
+    if(isOn === true){
+        header.style.display = "flex";
+        isOn = false;
+    }else{
+        header.style.display = "none";
+        isOn = true;
+    }
+});
+
+
+const eachProject = document.querySelectorAll(".each-project");
+
+for(const each of eachProject){
+    const counter = 0;
+    each.innerHTML = `
+    <img src="#" alt="wait lang" class="project-photo">
+    <p class="project-title">Nigga<p>
+    <p class="project-caption">"Website project for posting sakuga."<p>
+    `;
+}
