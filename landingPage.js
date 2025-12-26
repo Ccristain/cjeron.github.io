2c14e5545783f47e211394e4194a811072e930cc
.git <cristianjacoberon@gmail.com>
2025-12-26 15:03:44 +0800

    Skill cover

--- landingPage.js
+++ landingPage.js
@@ -80,3 +80,88 @@ function loadProjectCaption(param){
         break;
     }
 }
+
+/* SKILL ANIMATION */
+const coverText = {
+    hardSkillsText: {
+        "MS Office": {
+            title: "MS Office",
+            description: "Proficient in Microsoft Word and Excel for document creation."
+        },
+        "Web Development": {
+            title: "Web Development",
+            description: "Skilled in HTML, CSS, and JavaScript for building responsive websites."
+        },
+        "Graphic Design": {
+            title: "Graphic Design",
+            description: "Experienced in Canva for creating visual content."
+        },
+        "2d Animation": {
+            title: "2d Animation",
+            description: "Knowledgeable in Fundamentals for creating 2D animations."
+        },
+        "Network Configuration": {
+            title: "Network Configuration",
+            description: "Understanding of basic network setup and troubleshooting."
+        }
+    },
+    softSkillsText: {
+        "Problem Solving": {
+            title: "Problem Solving",
+            description: "Able to analyze issues and find effective solutions."
+        },
+        "Time Management": {
+            title: "Time Management",
+            description: "Efficient in organizing tasks to meet deadlines."
+        },
+        "Adaptability": {
+            title: "Adaptability",
+            description: "Flexible and open to change in dynamic environments."
+        },
+        "Goal Centric": {
+            title: "Goal Centric",
+            description: "Focused on achieving objectives and delivering results."
+        },
+        "Attentive": {
+            title: "Attentive",
+            description: "Paying close attention to details and instructions."
+        }
+    }
+}
+
+const hardSkills = document.querySelectorAll(".a-hardskill");
+const softSkills = document.querySelectorAll(".a-softskill");
+
+const hardSkillsCover = document.getElementById("hardskills-cover");
+const softSkillsCover = document.getElementById("softskills-cover");
+
+let hardTitle = document.getElementById("hard-title");
+let hardDescription = document.getElementById("hard-description");
+let softTitle = document.getElementById("soft-title");
+let softDescription = document.getElementById("soft-description");
+
+hardSkills.forEach((skill) => {
+    skill.addEventListener("mouseover", () => {
+        softSkillsCover.style.opacity = "100%";
+        softSkillsCover.style.zIndex = "1";
+        softTitle.innerText = coverText.hardSkillsText[skill.innerText].title;
+        softDescription.innerText = coverText.hardSkillsText[skill.innerText].description;
+        })
+    skill.addEventListener("mouseout", () => {
+        softSkillsCover.style.opacity = "0";
+        softSkillsCover.style.zIndex = "-1";
+    })
+});
+
+softSkills.forEach((skill) => {
+    skill.addEventListener("mouseover", () => {
+        hardSkillsCover.style.opacity = "100%";
+        hardSkillsCover.style.zIndex = "1";
+        hardTitle.innerText = coverText.softSkillsText[skill.innerText].title;
+        hardDescription.innerText = coverText.softSkillsText[skill.innerText].description;
+        })
+    skill.addEventListener("mouseout", () => {
+        hardSkillsCover.style.opacity = "0";
+        hardSkillsCover.style.zIndex = "-1";
+    })
+});
